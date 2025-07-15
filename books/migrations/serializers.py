9cf01fa16a479fbda69ab82ff8e59f370a36aee4
vllynnyk <magnetto54@gmail.com>
2025-07-15 10:42:13 +0200

    feat(books): add base and list serializers for Book model

---
+++ books/migrations/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from books.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "cover", "inventory", "daily_fee"]
+
+
+class BookListSerializer(BookSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author"]
