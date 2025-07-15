66d630362aff257b767d2ce52ff7cf0707cd060f
vllynnyk <magnetto54@gmail.com>
2025-07-15 11:19:53 +0200

    feat(books): add path to apps urls and register route for BookViewSet

---
+++ books/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from books.views import BookViewSet
+
+app_name = "books"
+router = DefaultRouter()
+router.register("", BookViewSet, basename="book")
+urlpatterns = [
+    path("", include(router.urls)),
+]
