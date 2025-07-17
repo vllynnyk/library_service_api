6cf8fc3b01f32d1103ba23f220a98c7809d135ba
vllynnyk <magnetto54@gmail.com>
2025-07-17 12:55:03 +0200

    feat(settings): add borrowings to INSTALLED_APPS and configure django-filter for API filtering

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework_simplejwt',
     "rest_framework",
+    'django_filters',
     "users",
     "books",
+    "borrowings",
 ]
 
 MIDDLEWARE = [
@@ -124,5 +126,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 AUTH_USER_MODEL = "users.User"
