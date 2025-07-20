9dffb432d22aa4e4c42077db97a60f98f22afdf4
vllynnyk <magnetto54@gmail.com>
2025-07-20 22:43:21 +0200

    feat(payments): create Payment model and register in admin

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,19 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+class Payment(models.Model):
+    class PaymentStatus(models.TextChoices):
+        PENDING = "Pending"
+        PAID = "Paid"
+    class PaymentType(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
+    status = models.CharField(max_length=10, choices=PaymentStatus.choices)
+    type = models.CharField(max_length=10, choices=PaymentType.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=55)
+    money_to_pay = models.DecimalField(decimal_places=2, max_digits=10)
