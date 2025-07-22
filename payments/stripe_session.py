e6650957a05ff1d32e4cb2345dbd90be777a10f4
vllynnyk <magnetto54@gmail.com>
2025-07-22 21:03:51 +0200

    fix(payments): improve function for removing errors

--- payments/stripe_session.py
+++ payments/stripe_session.py
@@ -1,25 +1,28 @@
-import os
 import stripe
-from dotenv import load_dotenv
+
 from django.urls import reverse
 from rest_framework.request import Request
 
 from borrowings.models import Borrowing
 from payments.models import Payment
 
-load_dotenv()
-
-stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
-
-
 def create_stripe_payment(request: Request, borrowing: Borrowing) -> Payment:
     price = borrowing.calculate_total_price()
+
+    payment = Payment.objects.create(
+        status=Payment.PaymentStatus.PENDING,
+        type=Payment.PaymentType.PAYMENT,
+        borrowing=borrowing,
+        money_to_pay=price,
+    )
+
     success_url = request.build_absolute_uri(
         reverse("payments:payments-success", kwargs={"pk": payment.id})
     )
     cancel_url = request.build_absolute_uri(
         reverse("payments:payments-cancel", kwargs={"pk": payment.id})
     )
+
     session = stripe.checkout.Session.create(
         success_url=success_url,
         cancel_url=cancel_url,
@@ -38,13 +41,8 @@ def create_stripe_payment(request: Request, borrowing: Borrowing) -> Payment:
         mode="payment",
     )
 
-    payment = Payment.objects.create(
-        status=Payment.Status.PENDING,
-        type=Payment.Type.PAYMENT,
-        borrowing=borrowing,
-        session_url=session.url,
-        session_id=session.id,
-        money_to_pay=price,
-    )
+    payment.session_url = session.url
+    payment.session_id = session.id
+    payment.save()
 
     return payment
